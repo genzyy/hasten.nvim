ba18ad7bcbab8a7b27b8fd962fea1b52d4ff2a4e
Rishit <rishpandey8097@gmail.com>
2024-01-27 20:11:44 +0530

    feat: basic switching buffers with custom keybinds

--- lua/switcher/init.lua
+++ lua/switcher/init.lua
@@ -3,14 +3,14 @@ local M = {}
 
 --- @param cfg table
 M.setup = function(cfg)
-	if cfg == nil then
-		print(vim.inspect("print set main key for switcher to work."))
-		return
-	end
-	if cfg.main_key == nil then
-		cfg.main_key = "<leader>" -- use leader as main key by default
-	end
-	commands.create_user_commands(cfg.main_key)
+  if cfg == nil then
+    print(vim.inspect("print set main key for switcher to work."))
+    return
+  end
+  if cfg.main_key == nil then
+    cfg.main_key = "<leader>" -- use leader as main key by default
+  end
+  commands.create_user_commands(cfg.main_key)
 end
 
 return M
